dc2e872190db15320302cfdc82c067bcaf6e7b56
anandureghu <anandukr145@gmail.com>
2024-07-12 11:39:02 +0530

    add subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,18 @@
-const Button = ({ label, icon, backgroundColor, borderColor, textColor }) => {
+const Button = ({
+  label,
+  icon,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
         ${backgroundColor ? backgroundColor : "bg-coral-red"}
         ${borderColor ? borderColor : "border-coral-red"}
         ${textColor ? textColor : "text-white"}
+        ${fullWidth && "w-full"}
         rounded-full`}
     >
       {label}{" "}
